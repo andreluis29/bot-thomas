27400782ded44636fbc2655ef20581c71adec7fe
andreluis29 <andre.wenningkamp1@gmail.com>
2022-06-09 15:33:38 -0300

    Descrição de comandos atualizada

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,8 +5,8 @@ const { clientId, guildId, token } = require('./config.json');
 
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('server').setDescription('Server status'),
+	new SlashCommandBuilder().setName('user').setDescription('Informações de usuário'),
 ]
 	.map(command => command.toJSON());
 
